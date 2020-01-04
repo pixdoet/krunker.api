3d0d53a44b72733f5867a7150fc9c4846846f6eb
Hoodgail <45743294+Hoodgail@users.noreply.github.com>
2020-01-04 12:46:28 -0800

    Add files via upload

---
+++ docs/list.js
@@ -0,0 +1,35 @@
+var list = {
+
+	"API": {
+
+		"Manual": {
+
+			"Getting Started": {
+				"Creating a page": "create_a_Page",
+    "How to get weapon id":"weapon_id",
+    "Loading KRUNKER.API":"loading"
+			},
+   	"Get": {
+				"Get items": "get_items",
+    "Get item's settings":"get_items_sett",
+			},
+   "ERRORS": {
+
+			"CORS": "cors"
+   },
+     "TEST": {
+
+			"3D iframe": "iframe3d_test"
+   },
+		
+
+
+		},
+
+
+
+	},
+
+	
+
+};
---
+++ examples/files.js
@@ -0,0 +1,5 @@
+var files = {
+	"basic": [
+		"_NOTHING_HERE",
+	]
+};
