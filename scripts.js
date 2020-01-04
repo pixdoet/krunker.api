86456d9ab435f3d8064c01ce2e9b4c849c726708
Hoodgail <45743294+Hoodgail@users.noreply.github.com>
2020-01-04 12:45:06 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+ 	"use strict";
+
+ 	$(window).load(function() {
+
+        // Preloader
+        $('.loader').fadeOut();
+        $('.loader-mask').delay(350).fadeOut('slow');
+		
+	});
+
+});
